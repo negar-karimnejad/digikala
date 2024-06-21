2b6d9509226b21d5e48a3afa106b95513ba87e81
negar <negar.karimnejad@gmail.com>
2024-06-21 12:38:52 +0330

    coding

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,7 +1,15 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
@@ -20,10 +28,4 @@ export async function POST(request: Request) {
     },
     status: 201,
   });
-}
-
-export async function DELETE(request: Request) {
-  const id = await request.json();
-  const filteredComments = comments.filter((comment) => comment.id !== id);
-  console.log(filteredComments);
 }
