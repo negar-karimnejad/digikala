4b51bb729815ce8711899648f1bf7b205346e5c1
negar <negar.karimnejad@gmail.com>
2024-06-22 12:59:12 +0330

    coding

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -39,7 +39,10 @@ export async function DELETE(
   const index = comments.findIndex(
     (comment) => comment.id === parseInt(params.id)
   );
+
   const deletedComment = comments[index];
+
   comments.splice(index, 1);
+  
   return Response.json(deletedComment);
 }
