4252aa0db683801bf522a89c1853f7bb7f209b90
negar <negar.karimnejad@gmail.com>
2024-07-03 19:08:09 +0330

    coding

--- src/app/admin/new/page.tsx
+++ src/app/admin/new/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Product } from "@prisma/client";
 import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
-import {
-  DownloadCloud,
-  LucideUploadCloud,
-  UploadCloud,
-  UploadCloudIcon,
-} from "lucide-react";
+import { LucideUploadCloud } from "lucide-react";
+import { useFormStatus } from "react-dom";
 import * as Yup from "yup";
 
 const initialValues: Product = {
@@ -25,6 +20,13 @@ const initialValues: Product = {
   orderId: null,
   rating: 0,
 };
+const imageSchema = Yup.mixed()
+  .test(
+    "is-valid-image",
+    "File must be an image or have zero size",
+    (file: any) => file.size === 0 || file.type?.startsWith("image/")
+  )
+  .test("is-file", "Required", (value: any) => value instanceof File);
 
 const ProductSchema = Yup.object().shape({
   title: Yup.string()
@@ -35,10 +37,23 @@ const ProductSchema = Yup.object().shape({
     .required("توضیحات محصول را وارد کنید"),
   price: Yup.number().required("قیمت محصول را وارد کنید"),
   discount: Yup.number().required("میزان تخفیف محصول را وارد کنید"),
-  thumbnail: Yup.string().required("تصویر محصول را وارد کنید"),
+  thumbnail: imageSchema
+    .test((file: any) => file.size > 0)
+    .required("لطفا تصویر را وارد کنید"),
 });
 
-export default function NewProduct() {
+
+
+type ContactFormInputs = TypeOf<typeof contactFormSchema>
+
+const contactFormSchema = object({
+  name: string({
+    required_error: "Please enter your name",
+  }),
+  email: string().email("Please enter a valid email"),
+  message: string().min(50).max(1000),
+});
+export default function ProductForm() {
   const submitHandler = async (
     values: Product,
     { resetForm }: FormikHelpers<Product>
@@ -46,11 +61,111 @@ export default function NewProduct() {
     console.log(values);
   };
 
-  const isLoading = false;
   return (
     <div className="">
       <h1 className="text-3xl font-bold mb-5">افزودن محصول جدید</h1>
-      <Formik
+
+      <Formik<ContactFormInputs>
+        initialValues={{
+          name: "",
+          email: "",
+          message: "",
+        }}
+        onSubmit={(values) => {
+          alert("Form is submitted");
+          console.log("Form is submitted", values);
+        }}
+        validationSchema={toFormikValidationSchema(contactFormSchema)}
+      >
+        {(FormikState) => {
+          const errors = FormikState.errors;
+          return (
+            <div className="card shadow">
+              <Form className="card-body">
+                <div className="card-title">Contact Us</div>
+                <div className="d-flex justify-content-between gap-2">
+                  <div className="form-group w-100">
+                    <label className="form-label">
+                      <span className="label-text">Your name</span>
+                    </label>
+                    <Field
+                      type="text"
+                      name="name"
+                      placeholder="e.g. John Doe"
+                      className="form-control"
+                    />
+                    {!!errors.name && (
+                      <div className="form-text text-danger">{errors.name}</div>
+                    )}
+                  </div>
+                  <div className="form-group w-100">
+                    <label className="form-label">
+                      <span className="label-text">Your email</span>
+                    </label>
+                    <Field
+                      type="text"
+                      name="email"
+                      placeholder="e.g johndoe@gmail.com"
+                      className="form-control"
+                    />
+                    {!!errors.email && (
+                      <div className="form-text text-danger">
+                        {errors.email}
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label className="form-label">
+                    <span className="label-text">Your message</span>
+                  </label>
+                  <Field
+                    as="textarea"
+                    type="text"
+                    name="message"
+                    className="form-control h-24"
+                    placeholder="Message"
+                  />
+                  {!!errors.message && (
+                    <div className="form-text text-danger">
+                      {errors.message}
+                    </div>
+                  )}
+                </div>
+                <div className="card-actions justify-content-end">
+                  <button className="btn btn-primary" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </Form>
+            </div>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+}
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      className="my-5 w-full text-lg font-medium"
+      disabled={pending}
+    >
+      <div className="flex items-center justify-center gap-2">
+        <span>{pending ? "در حال افزودن..." : "افزودن محصول"}</span>
+        {pending && (
+          <div className="h-5 w-5 animate-spin rounded-full border-2 border-dotted border-gray-800"></div>
+        )}
+      </div>
+    </Button>
+  );
+};
+
+{
+  /* <Formik
         initialValues={initialValues}
         validationSchema={ProductSchema}
         onSubmit={submitHandler}
@@ -88,7 +203,7 @@ export default function NewProduct() {
                 name="description"
                 as="textarea"
                 placeholder=""
-                className={`peer block w-full appearance-none rounded-t-lg border-0 border-b-2 border-gray-300 bg-gray-50 px-2.5 pb-2.5 pt-5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:bg-gray-900 dark:text-white dark:focus:border-blue-500 ${
+                className={`peer max-h-20 block w-full appearance-none rounded-t-lg border-0 border-b-2 border-gray-300 bg-gray-50 px-2.5 pb-2.5 pt-5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:bg-gray-900 dark:text-white dark:focus:border-blue-500 ${
                   touched.description && errors.description
                     ? "error-input border-b-2 border-red-500"
                     : ""
@@ -106,8 +221,8 @@ export default function NewProduct() {
                 معرفی محصول
               </label>
             </div>
-            <div className="flex gap-5 w-full">
-              <div className="h-20 relative w-full">
+            <div className="flex gap-5 w-full h-24">
+              <div className="relative w-full">
                 <Field
                   type="number"
                   id="price"
@@ -131,7 +246,7 @@ export default function NewProduct() {
                   قیمت محصول
                 </label>
               </div>
-              <div className="h-20 relative w-full">
+              <div className="relative w-full">
                 <Field
                   type="number"
                   id="discount"
@@ -156,8 +271,8 @@ export default function NewProduct() {
                 </label>
               </div>
             </div>
-            <div className="relative h-40 bg-blue-50 rounded-lg border-dashed border-2 border-gray-300">
-              <Input
+            <div className="relative h-40 bg-rose-50 dark:border-gray-700 dark:bg-gray-900 rounded-lg border-dashed border-2 border-gray-300">
+              <Field
                 type="file"
                 id="thumbnail"
                 name="thumbnail"
@@ -174,9 +289,11 @@ export default function NewProduct() {
                 component="div"
                 className="text-[11px] text-red-500"
               />
-              <div className="absolute top-0 bottom-0 left-0 right-0 m-auto flex items-center justify-center flex-col gap-2">
-                <LucideUploadCloud size={35} className="text-blue-600" />
-                <p className="font-bold text-gray-400">تصویر محصول را آپلود کنید</p>
+              <div className="flex -mt-32 items-center justify-center flex-col gap-2">
+                <LucideUploadCloud size={35} className="text-rose-600" />
+                <p className="font-medium text-gray-400">
+                  تصویر محصول را آپلود کنید
+                </p>
               </div>
               {/* {product != null && (
              <Image
@@ -185,22 +302,10 @@ export default function NewProduct() {
               width="400"
               alt="Product Image"
             />
-          )} */}
-            </div>
-            <Button
-              className="my-5 w-full text-lg font-bold"
-              // disabled={isLoading}
-            >
-              <div className="flex items-center justify-center gap-2">
-                <span>{isLoading ? "در حال افزودن..." : "افزودن محصول"}</span>
-                {isLoading && (
-                  <div className="h-5 w-5 animate-spin rounded-full border-2 border-dotted border-gray-800"></div>
-                )}
-              </div>
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+          )} 
+           </div>
+           <SubmitButton />
+         </Form>
+       )}
+     </Formik> */
 }
