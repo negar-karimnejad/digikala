4c779220a6c2b408fff77c594f941642157d14ec
negar <negar.karimnejad@gmail.com>
2024-07-11 21:04:13 +0330

    create not-found-page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import Image from "next/image";
+import Link from "next/link";
+import pageNotFound from "../../public/page-not-found.webp";
+import { ChevronLeft } from "lucide-react";
 function NotFound() {
   return (
-    <div>NotFound </div>
-  )
+    <div className="dark:bg-white w-full h-screen">
+      <div className="py-10 flex items-center justify-center flex-col">
+        <h2 className="lg:text-lg font-bold tracking-wider text-gray-800">
+          صفحه‌ای که دنبال آن بودید پیدا نشد!
+        </h2>
+        <Link
+          className="gap-1 mt-5 text-sky-500 text-sm flex items-center"
+          href="/"
+        >
+          صفحه اصلی
+          <ChevronLeft size={16} />
+        </Link>
+        <div className="lg:w-[550px] w-[300px] mt-10">
+          <Image
+            src={pageNotFound}
+            width={400}
+            height={400}
+            alt="not-found-page"
+            className="w-full h-full object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
