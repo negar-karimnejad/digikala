2d89d5e6fb746b6b2d18a8d5af7f3da6fe9568bc
negar <negar.karimnejad@gmail.com>
2024-07-12 11:02:10 +0330

    coding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu, ShoppingCart } from "lucide-react";
 import Link from "next/link";
+import Logo from "./Logo";
 import { DarkMode } from "./ui/DarkMode";
 import ProfileButton from "./ui/ProfileButton";
 import { Button } from "./ui/button";
 import Container from "./ui/container";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 const routes = [
   { label: "Products", href: "/" },
@@ -30,25 +33,27 @@ const NavLinks = () => {
 
 export default function Header() {
   return (
-    <header className="sm:flex sm:justify-between py-3 px-4 border-b">
+    <header className="sm:flex sm:justify-between py-3 border-b">
       <Container>
         <div className="flex justify-between items-center">
-          <div className="flex items-center gap-2">
-            <ProfileButton />
-            <DarkMode />
-            <Button variant="ghost" size="icon" aria-label="Shopping Cart">
-              <ShoppingCart />
-              <span className="sr-only">Shopping Cart</span>
-            </Button>
-          </div>
-          <nav className="max-md:hidden flex mx-6 items-center space-x-4 lg:space-x-6 ">
-            <NavLinks />
-          </nav>
           <div className="flex gap-2">
-            <h1 className="text-lg font-bold">
-              <Link href="/">STORE NAME</Link>
-            </h1>
-            <Sheet>
+            <div className="scale-75 mr-0 float-right">
+              <Logo />
+            </div>
+            <div>
+              <label
+                htmlFor="search"
+                className="w-[35rem] h-10 py-3 bg-gray-200 border-0"
+              >
+                <Input
+                  type="text"
+                  id="search"
+                  placeholder="جستجو"
+                  className="bg-gray-200 border-0 w-full"
+                />
+              </label>
+            </div>
+            {/* <Sheet>
               <SheetTrigger>
                 <Menu className="md:hidden" />
               </SheetTrigger>
@@ -57,7 +62,21 @@ export default function Header() {
                   <NavLinks />
                 </nav>
               </SheetContent>
-            </Sheet>
+            </Sheet> */}
+          </div>
+
+          {/* <nav className="max-md:hidden flex mx-6 items-center space-x-4 lg:space-x-6 ">
+            <NavLinks />
+          </nav> */}
+          <div className="flex items-center gap-2">
+            <DarkMode />
+            <div className="w-[0.5px] h-6 bg-gray-300 ml-2"></div>
+            <ProfileButton />
+            <div className="w-[0.5px] h-6 bg-gray-300 mr-2"></div>
+            <Button variant="ghost" size="icon" aria-label="Shopping Cart">
+              <ShoppingCart />
+              <span className="sr-only">Shopping Cart</span>
+            </Button>
           </div>
         </div>
       </Container>
