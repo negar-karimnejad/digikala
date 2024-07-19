0597796b9a4c94058f7a3329fd9ace682527bb91
negar <negar.karimnejad@gmail.com>
2024-07-19 21:22:45 +0330

    styled services card

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -1,27 +1,23 @@
 import { services } from "@/data/data";
 import Image from "next/image";
-import React from "react";
-import Container from "../ui/container";
 
 export default function Services() {
   return (
-    <Container>
-      <div className="flex justify-between items-start my-10">
-        {services.map((service) => (
-          <div
-            key={service.title}
-            className="flex flex-col w-full gap-2 justify-center"
-          >
-            <Image
-              alt={service.title}
-              width={50}
-              height={50}
-              src={service.image}
-            />
-            <p className="text-center text-sm w-10">{service.title}</p>
-          </div>
-        ))}
-      </div>
-    </Container>
+    <div className="flex justify-between items-start my-8 px-4 !mx-auto w-full">
+      {services.map((service) => (
+        <div
+          key={service.title}
+          className="flex flex-col w-full gap-2 justify-center items-center"
+        >
+          <Image
+            alt={service.title}
+            width={55}
+            height={55}
+            src={service.image}
+          />
+          <p className="text-center text-xs w-14 leading-6">{service.title}</p>
+        </div>
+      ))}
+    </div>
   );
 }
