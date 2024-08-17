053b23d394129fdb2dfce56db7dbb274a2ae27f8
negar <negar.karimnejad@gmail.com>
2024-08-17 22:10:12 +0330

    coding

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,14 +1,15 @@
 import AdminTable from "@/components/admin/AdminTable";
 import PageHeader from "@/components/admin/PageHeader";
 import db from "@/db/db";
+import useProducts from "@/hooks/useProducts";
 
 export default function AdminProductsPage() {
   return <ProductTable />;
 }
 
 async function ProductTable() {
-  const products = await db.product.findMany({ orderBy: { title: "asc" } });
-
+  // const products = await db.product.findMany({ orderBy: { title: "asc" } });
+  const { products } = useProducts();
   return (
     <>
       <PageHeader href="/admin/products/new" title="محصولات" />
