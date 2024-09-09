594cf64b341d2b365f5ed9514057fbdcfb051215
negar <negar.karimnejad@gmail.com>
2024-09-09 14:58:33 +0330

    coding articles

--- src/app/(client)/articles/page.tsx
+++ src/app/(client)/articles/page.tsx
@@ -1,6 +1,7 @@
 import { Article } from "@/types/types";
 import connectToDB from "configs/db";
 import ArticleModel from "models/Article";
+import Image from "next/image";
 
 export default async function ArticlesPage() {
   connectToDB();
@@ -10,9 +11,14 @@ export default async function ArticlesPage() {
       {articles.map((article) => (
         <div key={article._id.toString()}>
           <p>{article.title}</p>
+          <Image
+            src={article.cover}
+            width={100}
+            height={100}
+            alt={article.title}
+          />
         </div>
       ))}
     </div>
   );
 }
-
