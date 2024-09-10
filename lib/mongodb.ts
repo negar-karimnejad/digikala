5b5aeb3791f43b6a2e3e7b6a834f68856022569e
negar <negar.karimnejad@gmail.com>
2024-09-10 14:09:22 +0330

    coding

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,40 +1,16 @@
-import { MongoClient, MongoClientOptions } from "mongodb";
+const mongoose = require("mongoose");
 
-const uri = process.env.MONGODB_URI;
-
-const options: MongoClientOptions = {
-  serverSelectionTimeoutMS: 20000,
-  connectTimeoutMS: 20000,
-  socketTimeoutMS: 30000,
-  
+const connectToDB = async () => {
+  try {
+    if (mongoose.connections[0].readyState) {
+      return true;
+    } else {
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Connect To DB Successfully :))");
+    }
+  } catch (err) {
+    console.log("DB Connection has error ->", err);
+  }
 };
 
-let mongoClient: MongoClient | null = null;
-let database: any = null;
-
-if (!uri) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
-
-export default async function connectToDB() {
-  try {
-    if (mongoClient && database) {
-      return { mongoClient, database };
-    }
-    if (process.env.NODE_ENV === "development") {
-      if (!global._mongoClient) {
-        mongoClient = await new MongoClient(uri, options).connect();
-        global._mongoClient = mongoClient;
-      } else {
-        mongoClient = global._mongoClient;
-      }
-    } else {
-      mongoClient = await new MongoClient(uri, options).connect();
-    }
-    database = mongoClient.db(process.env.NEXT_ATLAS_DATABASE);
-    return { mongoClient, database };
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
-}
+export default connectToDB;
