5fda4712f67527e2f92e2a9b24685a0b1f0ac282
negar <negar.karimnejad@gmail.com>
2024-09-14 23:44:01 +0330

    create submenu products page

--- src/app/(client)/category/[...id]/page.tsx
+++ src/app/(client)/category/[...id]/page.tsx
@@ -1,146 +1,18 @@
-import CategoryHero from "@/components/category/CategoryHero";
-import Articles from "@/components/home/Articles";
-import Bestseller from "@/components/home/Bestseller";
-import Brands from "@/components/home/Brands";
-import Offers from "@/components/home/Offers";
-import SelectedProducts from "@/components/home/SelectedProducts";
-import { Category, Product } from "@/types/types";
-import { serializeDoc } from "@/utils/serializeDoc";
-import ArticleModel from "models/Article";
-import CategoryModel from "models/Category";
-import ProductModel from "models/Product";
-import Image from "next/image";
-import Link from "next/link";
+import CategoryProducts from "@/components/category/categoryProducts";
+import SubmenuProducts from "@/components/category/SubmenuProducts";
 
 export default async function CategoryPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const category: Category = await CategoryModel.findOne({
-    href: `/category/${id}`,
-  })
-    .populate({
-      path: "submenus",
-      populate: {
-        path: "items",
-      },
-    })
-    .lean();
-
-  const articles = await ArticleModel.find({ categoryId: category?._id }).lean();
-  const products: Product[] = await ProductModel.find({})
-    .populate({
-      path: "category",
-      populate: {
-        path: "submenus",
-        populate: {
-          path: "items",
-        },
-      },
-    })
-    .lean();
-
-  // Category Products
-  const categoryProducts = products.filter(
-    (product) => product.category.href === `/category/${id}`
-  );
-
-  // Discount Products
-  const discountProducts = categoryProducts.filter(
-    (product) => product.discount > 0
-  );
-
-  const offerProducts = discountProducts
-    ?.slice()
-    .sort((a, b) => b.discount - a.discount)
-    .slice(0, 12);
-
-  // Mapping submenus to their first product's thumbnail
-  const submenuProductImages = category?.submenus.map((submenu) => {
-    // Find the first product associated with the current submenu
-    const firstProduct = categoryProducts.find(
-      (product) => product.submenuId === submenu._id.toString()
-    );
-    return firstProduct?.thumbnail || "";
-  });
-
-  const serializedCategory = serializeDoc(category);
-  const serializedcategoryProducts = serializeDoc(categoryProducts);
-  const serializedArticles = serializeDoc(articles);
-
-  if (!category) return null;
   return (
-    <div className="space-y-10">
-      <CategoryHero category={serializedCategory} />
-      <Offers products={offerProducts} />
-
-      {/* Shopping by category */}
-      {category.submenus.length > 0 && (
-        <div>
-          <h3 className="font-irsansb text-lg text-center">
-            خرید بر اساس دسته‌بندی
-          </h3>
-          <div className="mt-10 flex gap-10 items-center justify-center flex-wrap">
-            {category.submenus?.map((submenu, index) =>
-              submenuProductImages[index] ? (
-                <div key={submenu._id.toString()}>
-                  <Link
-                    href={submenu.href}
-                    className="relative mb-5 flex flex-col items-center gap-4"
-                  >
-                    <Image
-                      alt={submenu.title}
-                      width={140}
-                      height={140}
-                      src={submenuProductImages[index]}
-                    />
-                    <p className="text-sm">{submenu.title}</p>
-                  </Link>
-                </div>
-              ) : null
-            )}
-          </div>
-        </div>
+    <>
+      {id.length === 1 ? (
+        <CategoryProducts id={id} />
+      ) : (
+        <SubmenuProducts id={id} />
       )}
-
-      <Brands />
-
-      {/* Banners */}
-      <div className="grid grid-cols-12 gap-4 mx-3">
-        {category.banner?.map((item, index) => (
-          <div className="col-span-6 max-lg:col-span-12" key={index}>
-            <Link href="">
-              <Image
-                width={1700}
-                height={1700}
-                className="rounded-xl object-cover w-full"
-                src={item}
-                alt={`Banner ${index + 1}`}
-              />
-            </Link>
-          </div>
-        ))}
-      </div>
-
-      <Bestseller
-        products={serializedcategoryProducts}
-        title="پرفروش‌ترین کالاها"
-      />
-      <div>
-        <Image
-          alt="زنگ تفریح دیجی‌کالا"
-          title="زنگ تفریح دیجی‌کالا"
-          width={2000}
-          height={500}
-          src="/069665291f9d877a19922f0c741e7620ad85cf6b_1725653469.gif"
-        />
-      </div>
-      <SelectedProducts products={serializedcategoryProducts} />
-
-      {articles.length > 0 && (
-        <Articles articles={serializedArticles} title="مطالب مرتبط" />
-      )}
-    </div>
+    </>
   );
 }
